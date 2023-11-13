5220e027a657d772941e28c926054fdd85895015
elisacch <elisa.sacchelli@icloud.com>
2023-11-13 10:41:04 +0000

    Set up firestore tools

--- src/components/ResultTable.js
+++ src/components/ResultTable.js
@@ -1,9 +1,13 @@
+// hooks
+import { useFirestore } from '../hooks/useFirestore';
 //styles
 import styles from './styles/ResultTable.module.css';
 
 export default function ResultTable({ data }) {
+    const isThereData = data.length > 0;
 
-    const isThereData = data.length > 0
+    const { deleteDocument } = useFirestore('hours');
+
     return (
         <>
             {isThereData && (
@@ -17,6 +21,7 @@ export default function ResultTable({ data }) {
                                 <th>Time in</th>
                                 <th>Time out</th>
                                 <th>Total</th>
+                                <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -30,6 +35,14 @@ export default function ResultTable({ data }) {
                                             <td>{doc.timeIn}</td>
                                             <td>{doc.timeOut}</td>
                                             <td>{doc.total}</td>
+                                            <td>
+                                                <button onClick={() => deleteDocument(doc.id)} className={styles["material-symbols-outlined"]}>
+                                                    <span className="material-symbols-outlined">
+                                                        delete
+                                                    </span>
+                                        
+                                                </button>
+                                            </td>
                                         </tr>
                                     )
                                 )
--- src/hooks/useFirestore.js
+++ src/hooks/useFirestore.js
@@ -19,6 +19,8 @@ const firestoreReducer = (state, action) => {
             return { isPending: true, document: null, success: false, error: null }
         case 'ADDED_DOC':
             return { isPending: false, document: action.payload, success: true, error: null }
+        case 'DELETED_DOCUMENT':
+            return { isPending: false, document: null, success: true, error: null }
         case 'ERROR':
             return { isPending: false, document: null, success: false, error: action.payload }
         default:
@@ -50,12 +52,20 @@ export const useFirestore = (collection) => {
             const addedDocument = await ref.add({...doc, total, createdAt});
             dispatchIfNotCancelled({ type: 'ADDED_DOC', payload: addedDocument });
         } catch (err) {
-            console.log(err);
             dispatchIfNotCancelled({ type: 'ERROR', payload: err.message });
         }
     }
     // delete document
-    const deleteDocument = id => { }
+    const deleteDocument = async id => {
+        dispatch({ type: 'IS_PENDING' });
+        try {
+            await ref.doc(id).delete();
+            dispatchIfNotCancelled({ type: 'DELETED_DOCUMENT' })
+        } catch (err) {
+            console.log(err);
+            dispatchIfNotCancelled({ type: 'ERROR', payload: 'There was an error deleting this document.'})
+        }
+    }
 
     useEffect(() => {
         setIsCancelled(false);
